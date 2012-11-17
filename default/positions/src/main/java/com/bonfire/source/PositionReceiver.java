3cf34e2b3ca7d26b208fdf4f4afd5c612cc03a9c
Vinith Jacob Philipose <its.v.j.p@gmail.com>
2012-11-16 18:46:55 -0800

    re-factor and create console observer

--- default/positions/src/main/java/com/bonfire/source/PositionReceiver.java
+++ default/positions/src/main/java/com/bonfire/source/PositionReceiver.java
@@ -1,12 +1,18 @@
-package main;
+package com.bonfire.source;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Observable;
 
-public class PositionReceiver {
+public class PositionReceiver extends Observable{
 	
 	public static void main(String[] args){
+		PositionReceiver positionReceiver = new PositionReceiver();
+		positionReceiver.start();
+	}
+
+	private void start() {
 		BufferedReader consoleReader = null;
 		try{
 			consoleReader = new BufferedReader(new InputStreamReader(System.in));
@@ -14,6 +20,7 @@ public class PositionReceiver {
 			
 			while(inputString==null || !inputString.equals("quit")){
 				inputString = consoleReader.readLine();
+				notifyObservers(inputString);
 			}
 		}catch(IOException e){
 			e.printStackTrace();
@@ -21,7 +28,6 @@ public class PositionReceiver {
 			try {
 				consoleReader.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
