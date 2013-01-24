a7f02adbc464846f3f7025f711f7fed415272250
VJP <its.v.j.p@gmail.com>
2013-01-23 22:06:39 -0800

    adding data to the table

--- default/com.bonfire.order.view/src/com/bonfire/order/view/parts/DetailsPart.java
+++ default/com.bonfire.order.view/src/com/bonfire/order/view/parts/DetailsPart.java
@@ -6,17 +6,25 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 
 public class DetailsPart {
 	@PostConstruct
 	  public void createControls(Composite parent) {
 		Table table = new Table(parent, SWT.MULTI/* | SWT.BORDER | SWT.FULL_SELECTION*/);
 		table.setHeaderVisible (true);
+		table.setLinesVisible(true);
 		String[] columns = new String[]{"ID", "Side", "Symbol", "Qty", "Cum Qty"};
+		String[] data = new String[]{"Order-1", "Buy", "6758.T", "1000", "200"};
 		
 		for(String column:columns){
 			TableColumn tableColumn = new TableColumn(table, SWT.LEFT);
 			tableColumn.setText(column);
+		}
+		
+		for(String datum:data){
+			TableItem tableItem = new TableItem(table, SWT.LEFT);
+			tableItem.setText(data);
 		}
 		
 		for(TableColumn tableColumn:table.getColumns()){
