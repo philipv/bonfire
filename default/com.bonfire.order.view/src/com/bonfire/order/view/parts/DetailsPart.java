7d982c4b9dc82d7a60ce015a3e324e030a93a02e
its.v.j.p@gmail.com <its.v.j.p@gmail.com>
2013-01-24 16:42:15 +0800

    adding row selection

--- default/com.bonfire.order.view/src/com/bonfire/order/view/parts/DetailsPart.java
+++ default/com.bonfire.order.view/src/com/bonfire/order/view/parts/DetailsPart.java
@@ -10,24 +10,26 @@ import org.eclipse.swt.widgets.TableItem;
 
 public class DetailsPart {
 	@PostConstruct
-	  public void createControls(Composite parent) {
-		Table table = new Table(parent, SWT.MULTI/* | SWT.BORDER | SWT.FULL_SELECTION*/);
-		table.setHeaderVisible (true);
+	public void createControls(Composite parent) {
+		Table table = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
-		String[] columns = new String[]{"ID", "Side", "Symbol", "Qty", "Cum Qty"};
-		String[] data = new String[]{"Order-1", "Buy", "6758.T", "1000", "200"};
-		
-		for(String column:columns){
+		String[] columns = new String[] { "ID", "Side", "Symbol", "Qty",
+				"Cum Qty" };
+		String[] data = new String[] { "Order-1", "Buy", "6758.T", "1000",
+				"200" };
+
+		for (String column : columns) {
 			TableColumn tableColumn = new TableColumn(table, SWT.LEFT);
 			tableColumn.setText(column);
 		}
-		
-		for(String datum:data){
+
+		for (int i=0;i<5;i++) {
 			TableItem tableItem = new TableItem(table, SWT.LEFT);
 			tableItem.setText(data);
 		}
-		
-		for(TableColumn tableColumn:table.getColumns()){
+
+		for (TableColumn tableColumn : table.getColumns()) {
 			tableColumn.pack();
 		}
 	}
