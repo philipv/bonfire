f2756ef55f14b3b600c303b6d819de379d1c39e0
its.v.j.p@gmail.com <its.v.j.p@gmail.com>
2013-01-24 17:01:51 +0800

    test lifecycle

--- default/com.bonfire.order.view/src/com/bonfire/order/view/parts/OverviewPart.java
+++ default/com.bonfire.order.view/src/com/bonfire/order/view/parts/OverviewPart.java
@@ -1,7 +1,17 @@
 package com.bonfire.order.view.parts;
 
+import javax.inject.Inject;
+
+import org.eclipse.swt.widgets.Composite;
+
 public class OverviewPart {
 	public OverviewPart(){
 		System.out.println("Creating OverviewPart");
+	}
+	
+	@Inject
+	public OverviewPart(Composite parent){
+		System.out.println("Got composite via DI.");
+		System.out.println("Layout: " + parent.getLayout().getClass());
 	}
 }
