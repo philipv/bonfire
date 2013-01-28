713b625da91706c0732c28c069f2acf6919355e3
its.v.j.p@gmail.com <its.v.j.p@gmail.com>
2013-01-28 16:53:10 +0800

    adding a command and its respective handler

---
+++ default/com.bonfire.order.view/src/com/bonfire/order/view/handlers/TestHandler.java
@@ -0,0 +1,17 @@
+package com.bonfire.order.view.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class TestHandler {
+
+	@Execute
+	  public void execute(@Named("com.bonfire.order.view.commandparameter.input")String param) {
+	    System.out.println("Calling the testhandler with param : " + param);
+	  }
+	/*@Execute
+	public void execute() {
+		System.out.println("Calling the testhandler");
+	}*/
+}
--- default/com.bonfire.order.view/src/com/bonfire/order/view/parts/OverviewPart.java
+++ default/com.bonfire.order.view/src/com/bonfire/order/view/parts/OverviewPart.java
@@ -1,10 +1,16 @@
 package com.bonfire.order.view.parts;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 public class OverviewPart {
+	private Label label;
 	public OverviewPart(){
 		System.out.println("Creating OverviewPart");
 	}
@@ -13,5 +19,21 @@ public class OverviewPart {
 	public OverviewPart(Composite parent){
 		System.out.println("Got composite via DI.");
 		System.out.println("Layout: " + parent.getLayout().getClass());
+	}
+	
+	@PostConstruct
+	public void createControls(Composite parent) {
+		label = new Label(parent,SWT.NONE);
+		label.setText("A text....");
+	} 
+	
+	@Focus
+	private void setFocus() {
+		label.setFocus();
+	} 
+	
+	@PreDestroy
+	public void dispose(){
+		System.out.println("Closing OverviewPart!!");
 	}
 }
