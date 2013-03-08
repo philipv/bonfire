4ac10ca1dbd2aa39dcd387aa4083de6c98e743bb
its.v.j.p@gmail.com <its.v.j.p@gmail.com>
2013-03-08 17:56:11 +0800

    fixed the unresolvable pointcut issue by changing the version of aspectj

--- default/springaop/src/main/java/bonfire/springaop/aspects/BasicAdvice.java
+++ default/springaop/src/main/java/bonfire/springaop/aspects/BasicAdvice.java
@@ -5,8 +5,8 @@ import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class BasicAdvice {
-	//@Before("bonfire.springaop.aspects.BasicAspect.anyOldTransfer()")
-	@Before("execution(* transfer(..))")
+	@Before("bonfire.springaop.aspects.BasicAspect.anyOldTransfer()")
+	//@Before("execution(* transfer(..))")
 	public void doAccessCheck() {
 		System.out.println("This is the proof that the advice ran");
 	}
