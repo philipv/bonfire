77c503b576967eaed14e8154271dafb1434f627f
its.v.j.p@gmail.com <its.v.j.p@gmail.com>
2013-03-26 11:30:10 +0800

    adding a string based auto wiring just to see if it works.

--- default/springaop/src/main/java/bonfire/springaop/targets/MySaveTarget.java
+++ default/springaop/src/main/java/bonfire/springaop/targets/MySaveTarget.java
@@ -1,13 +1,20 @@
 package bonfire.springaop.targets;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class MySaveTarget {
 	private static final Logger logger = LoggerFactory.getLogger(MySaveTarget.class.getCanonicalName());
 	
+	@Autowired
+	private List<String> testTarget;
+	
 	public void save(){
-		logger.info("save called");
+		logger.info("save called with field {}", testTarget);
 	}
+	
 }
